1596fb166e3e89045f4f207c914103bdfc17bbb1
LeandroBarea <barealeandro24@gmail.com>
2026-05-12 03:44:43 -0300

    Agrego componentes de Layout: Header, Nav y Footer Leandro Barea

---
+++ pvtp03GrupoN01/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer-tp">
+      <p><b>© 2026 Proyectos Educativos - Todos los derechos reservados</b></p>
+      <p><b>Correo: ProyectosEducativos@gmail.com</b></p>
+      <p><b>Telefono: 11111</b></p>
+    </footer>
+  );
+};
+
+export default Footer;
