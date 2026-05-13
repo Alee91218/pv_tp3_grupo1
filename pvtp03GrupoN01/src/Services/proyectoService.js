81e6bdcc4ecc88446242259b47b88295efd720d1
42803698@fi.unju.edu.ar <42803698@fi.unju.edu.ar>
2026-05-13 03:58:46 -0300

    Modificaciones necesarias para el funcionamiento correcto del codigo y agregue la funcion agregar proyecto - Alejandro Fernandez

--- pvtp03GrupoN01/src/Services/proyectoService.js
+++ pvtp03GrupoN01/src/Services/proyectoService.js
@@ -1,48 +1,35 @@
-let proyectos = [
-    { 
-        id: 1, 
-        titulo: "Plataforma Educativa", 
-        categoria: "Programación", 
-        estado: "En curso" 
-    },
-    { 
-        id: 2, 
-        titulo: "App de Tareas", 
-        categoria: "Productividad", 
-        estado: "Completado" 
-    },
-    { 
-        id: 3, 
-        titulo: "Huerto Escolar", 
-        categoria: "Biología", 
-        estado: "En curso" 
-    },
-    { 
-        id: 4, 
-        titulo: "Robótica Educativa", 
-        categoria: "Ingeniería", 
-        estado: "En curso" 
-    },
-    { 
-        id: 5, 
-        titulo: "ATaller Deportivo", 
-        categoria: "Educacion Física", 
-        estado: "Completado" 
-    }
+const proyectService = (() => {
+    let proyectos = [
+        { id: 1, titulo: "Plataforma Educativa", categoria: "Programación", estado: true },
+        { id: 2, titulo: "App de Tareas", categoria: "Productividad", estado: false },
+        { id: 3, titulo: "Huerto Escolar", categoria: "Biología", estado: false },
+        { id: 4, titulo: "Robótica Educativa", categoria: "Ingeniería", estado: true },
+        { id: 5, titulo: "ATaller Deportivo", categoria: "Educacion Física", estado: false }
 ];
 
-export const obtenerProyectos = () => [...proyectos];
+    const obtenerProyectos = () => {
+     return[...proyectos];
+    };
 
-export const agregarProyecto = (nuevoProyecto) => {
-    proyectos.push(nuevoProyecto);
-};
+    const agregarProyecto = (p) => {
+        const nuevoProyecto = {...p, id: Date.now()};
+        proyectos.push(nuevoProyecto);
+    };
 
-export const eliminarProyecto = (id) => {
-    proyectos = proyectos.filter(proyecto => proyecto.id !== id);
-};
+    const eliminarProyecto = (id) => {
+        proyectos = proyectos.filter(proyecto => proyecto.id !== id);
+    };
 
-export const buscarProyecto = (texto) => {
-    return proyectos.filter(proyecto => 
-        proyecto.titulo.toLowerCase().includes(texto.toLowerCase())
-    );
-};
+    const buscarProyecto = (texto) => {
+        return proyectos.filter(proyecto => 
+        proyecto.titulo.toLowerCase().includes(texto.toLowerCase()));
+    };
+    return {
+        obtenerProyectos,
+        agregarProyecto,
+        eliminarProyecto,
+        buscarProyecto
+    };
+})();
+
+export default proyectService;
