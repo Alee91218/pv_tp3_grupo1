0e4c8d457cce31d87d1fb70085de3379ddb10a19
42803698@fi.unju.edu.ar <42803698@fi.unju.edu.ar>
2026-05-21 00:38:32 -0300

    Ultima modificacion - Alejandro Fernandez

--- pvtp03GrupoN01/src/components/ListaProyectos.jsx
+++ pvtp03GrupoN01/src/components/ListaProyectos.jsx
@@ -1,9 +1,4 @@
-// CAMBIO IMPORTANTE: Corregida la ruta del import con un solo punto './' porque ya estamos dentro de components
-import ProyectoCard from './ProyectoCard.jsx'; 
-
-// CAMBIO IMPORTANTE: Recibimos onEliminar y onBuscar desestructurados desde las props del padre
-const ListaProyectos = ({ proyectos, verDetalle, onEliminar, onBuscar }) => {
-
+import CardProject from "./ProyectoCard.jsx";
 
 const ListProject = ({ proyectos, verDetalle,eliminarProyecto}) => {
   if(proyectos.length === 0) return <p>No hay proyectos registrados</p>
@@ -16,9 +11,10 @@ const ListProject = ({ proyectos, verDetalle,eliminarProyecto}) => {
                     proyecto={proyecto}
                     eliminarProyecto={eliminarProyecto}
                     verDetalle={verDetalle}
-
+                />
+            ))}
         </section>
     );
-};
 
-export default ListaProyectos;
+};
+export default ListProject;
