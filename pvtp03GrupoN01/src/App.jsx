41e63f06f7fe400ad63deb33316a42efdea0ef32
FrancoPonce579 <poncefranco957@gmail.com>
2026-06-03 20:02:50 -0300

    Solucion de errores en dashboard Franco Ponce

--- pvtp03GrupoN01/src/App.jsx
+++ pvtp03GrupoN01/src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/Header' 
 import Footer from './components/Footer' 
 import Proyectos from './components/Proyecto'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import PerfilUsuario from './views/PerfilUsuario';
 import Dashboard from './views/Dashboard';
 import DetalleProyecto from './components/DetalleProyecto'; // CAMBIO LEANDRO: Importación de tu sección independiente
@@ -13,11 +13,16 @@ function App() {
     <Header />
     <main>  
       <Routes>
-        <Route 
-          path="/"
-          element={<Dashboard/>}
+        <Route
+         path="/"
+         element={<Navigate to="/dashboard" />}
         />
 
+        <Route 
+          path="/dashboard"
+          element={<Dashboard/>}
+        />
+        
          <Route 
           path="/perfil"
           element={ <PerfilUsuario/> }
